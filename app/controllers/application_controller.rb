ac3667718ed39c99b1048167c900352445a6b989
Keith Schacht <krschacht@gmail.com>
2010-02-16 00:05:17 -0600

    resolve merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,27 @@
 
 class ApplicationController < ActionController::Base
 
+  helper :all
+
+  helper_method :facebook_session, 
+                :api_key, 
+                :xd_receiver,
+                :current_user,
+                :action_name_safe
+
+  attr_reader   :current_user
+
   ensure_authenticated_to_facebook
   ensure_application_is_installed_by_facebook_user
 
-  before_filter :set_facebook_session
-  helper_method :facebook_session, :api_key, :xd_receiver
+  before_filter :set_facebook_session,
+                :set_current_user,
+                :log_user_ids,
+                :set_ie7_header,
+                :update_install,
+                :update_install_source,
+                :select_tab
 
-  helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   def api_key
@@ -18,6 +32,62 @@ class ApplicationController < ActionController::Base
 
   def xd_receiver
     "/xd_receiver.htm"
+  end
+
+  def set_current_user
+    if facebook_session
+      @current_user = User.find_or_create_by_facebook_id(facebook_session.user.id)
+    end
+  end
+
+  def log_user_ids
+    if current_user
+      logger.info "*** User: #{current_user.id} (facebook_id: #{current_user.facebook_id})"
+    end
+  end
+  
+  def set_ie7_header
+    response.headers["X-UA-Compatible"] = 'IE=7'
+  end
+  
+  # If the user has installed or removed the app since the last request
+  # update the install / remove date.
+
+  def update_install
+    if current_user && request_comes_from_facebook?
+      if ! current_user.installed? && facebook_params[:added]
+        current_user.install
+      elsif current_user.installed? && ! facebook_params[:added]
+        current_user.remove
+      end
+    end
+  end
+
+  # Check params for install_source and save to current_user.  We only record
+  # install_source if it hasn't been previously set and the user is relatively
+  # new.
+
+  def update_install_source
+    s = params[:install_source] || params[:s]
+
+    if  current_user && s && ! current_user.played?
+      
+      current_user.update_attributes( 
+        { :install_source => s, 
+          :created_at => Time.now 
+        } )
+
+    end
+  end
+
+  def action_name_safe
+    # Eliminate any invalid iv characters from action.
+    params[:action].gsub( /\W/, '' )
+  end
+
+  def select_tab
+    instance_variable_set( "@#{controller_name}_tab", true )
+    instance_variable_set( "@#{controller_name}_#{action_name_safe}_tab", true )
   end
 
   # Scrub sensitive parameters from your log
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,35 @@ module ApplicationHelper
     facebooker_session.instance_variable_set("@secret_key",'xxxxxxxxxxxxxxxxxxx')
     facebooker_session.to_yaml
   end
+  
+  def find_session_by_id( sid )
+    defined?( SESSION_CACHE ) ? SESSION_CACHE.get( sid ) || {} : {}
+  end
+  
+  def session_id
+    request.session_options[:id]
+  end
+  
+  def tab( text, opts={}, html_opts={} )
+    href     = opts.delete(:href)
+    classes  = ""
+    classes += " first"   if opts.delete(:first)
+    classes += " last"    if opts.delete(:last)
+
+    html_opts[:class] ||= ""
+    html_opts[:class] += " selected"  if opts.delete(:selected)
+
+    # Set these as defaults to save typing :)
+    opts[:host]         = 'apps.facebook.com'
+    opts[:only_path]    = false
+    opts[:canvas]       = true
+    html_opts[:target]  = '_top'
+
+    if href
+      %Q(<li class="#{classes}">#{link_to text, href, opts, html_opts}</li>)
+    else
+      %Q(<li class="#{classes}">#{link_to text, opts, html_opts}</li>)
+    end
+  end
+  
 end
