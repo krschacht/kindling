71cb78dbab893fe64f282ecf654ab910ea167249
Keith Schacht <krschacht@gmail.com>
2010-02-16 00:00:32 -0600

    clean-up javascript & login stubs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,60 +3,60 @@
 var facebook_api = null;
 
 $(document).ready( function() {
-                       // Just because the document is loaded doesn't mean the FB
-                       // library has finished loading. Including all the usual onready()
-                       // stuff inside this callback.
+  // Just because the document is loaded doesn't mean the FB
+  // library has finished loading. Including all the usual onready()
+  // stuff inside this callback.
 
 
-                       FB.init( api_key, xd_receiver );
+  FB.init( api_key, xd_receiver );
 
-                       FB.ensureInit(function() {
-                                         facebook_api = FB.Facebook.apiClient;
+  FB.ensureInit(function() {
+    facebook_api = FB.Facebook.apiClient;
 
-                                         try {
-                                             if( top.location.host == undefined ) {
-                                                 throw( "error" );
-                                             }
-                                             //## Outside Facebook
+    try {
+       if( top.location.host == undefined ) {
+           throw( "error" );
+       }
+       //## Outside Facebook
 
-                                             if( top.location.pathname == "/chat" ) {
-                                                 //## Outside Facebook but inside our chat frame
+       if( top.location.pathname == "/chat" ) {
+           //## Outside Facebook but inside our chat frame
 
-                                             } else {
-                                                 //## Outside Facebook not in any frames
+       } else {
+           //## Outside Facebook not in any frames
 
-                                             }
+       }
 
-                                         } catch( err ) {
-                                             //## Inside a Facebook iframe
+    } catch( err ) {
+       //## Inside a Facebook iframe
 
-                                             FB.CanvasClient.startTimerToSizeToContent();
-                                             scroll_to_top();
-                                         }
+       FB.CanvasClient.startTimerToSizeToContent();
+    }
 
-                                         check_cookie();
-                                     }); // ensureInit
-                   }); // ready
+    check_cookie();
+    }); // ensureInit
+}); // ready
 
 function check_cookie() {
-    if( typeof session_cookie != "undefined" &&
-        typeof session_id     != "undefined" ) {
-            if( $.cookie( session_cookie ) != session_id ) {
-                /*
-                 if( $.browser.mozilla ) {
-                 $('body').html( $('#third_party_cookies_disabled').html() );
-                 }
-                 else {
-                 */
-                fix_cookie_with_ajax( function() {
-                                          if( $.cookie( session_cookie ) != session_id ) {
-                                              fix_cookie_with_form();
-                                          }
-                                      } );
-                /*
-                 }
-                 */
-            }
-        }
+  if( typeof session_cookie != "undefined" &&
+      typeof session_id     != "undefined" ) {
+
+    if( $.cookie( session_cookie ) != session_id ) {
+      /*
+       if( $.browser.mozilla ) {
+       $('body').html( $('#third_party_cookies_disabled').html() );
+       }
+       else {
+       */
+      fix_cookie_with_ajax( function() {
+                                if( $.cookie( session_cookie ) != session_id ) {
+                                    fix_cookie_with_form();
+                                }
+                            } );
+      /*
+       }
+       */
+    }
+  }
 }
 
